139be95df0f0e62ad78bb46c2e13a367eda068c9
Cameron-Stock <clstock33@gmail.com>
2020-11-21 20:44:08 -0600

    Init commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68); 
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20); 
+
+            Console.WriteLine("After conversion:");
+
+            Console.WriteLine($"celcius {celsius}");
+
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+            //Static exercise
+        }
+    }
+}
